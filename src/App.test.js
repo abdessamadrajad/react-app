fdf067be51a35beae046da9f58fc8bdc524b0e76
sadmanrajad <abdessamadrajad@gmail.com>
2020-03-15 22:10:21 +0100

    added travis file3

--- src/App.test.js
+++ src/App.test.js
@@ -9,8 +9,3 @@ test('renders learn react link', () => {
 });
 
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
