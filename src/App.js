a365d797575a6f64c234b1086744e7d2a79d7f15
sadmanrajad <abdessamadrajad@gmail.com>
2020-03-16 17:27:00 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SARAHA
+          CORONAVIRUS is HERE
         </p>
         <a
           className="App-link"
